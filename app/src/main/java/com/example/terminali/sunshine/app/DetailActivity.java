0121e9bcae0e41a6695dd3fc0ed8be1c06a75475
LinuxTerminali <nkmishra1995@gmail.com>
2016-07-15 17:38:47 +0530

    Initial commit

---
+++ app/src/main/java/com/example/terminali/sunshine/app/DetailActivity.java
@@ -0,0 +1,34 @@
+package com.example.terminali.sunshine.app;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+
+public class DetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        /*FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });*/
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    @Override
+    public Boolean onCreateOptionsMenu(Menu menu){
+        getMenuInflater().inflate(R.menu.menu_detail,menu);
+    }
+
+
+}
